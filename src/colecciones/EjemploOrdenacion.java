cacb715ee3cfdc53cbe7c1b127cb2d3807071ab1
diegoProFP <diego.di1@educa.madrid.org>
2026-04-08 12:29:48 +0200

    ejempl,os ordenacion

--- src/colecciones/EjemploOrdenacion.java
+++ src/colecciones/EjemploOrdenacion.java
@@ -24,5 +24,44 @@ public class EjemploOrdenacion {
         //Ordenamos la lista de forma INVERSA
         numeros.sort(Comparator.reverseOrder());
         System.out.println(numeros);
+
+        List<Estudiante> estudiantesErasmus = Arrays.asList(
+                new Estudiante(1, "Johan", 5.2, "111111A"),
+                new Estudiante(2, "Alice", 9.1, "222222A"),
+                new Estudiante(3, "Bob", 9.1, "0000000A")
+        );
+
+        System.out.println("Los estudiantes Erasmus son: ");
+        //Collections.sort(estudiantesErasmus);
+
+        //Ejemplo, queremos comparar por otro campo, el numero estudiante:
+        estudiantesErasmus.sort(Comparator.comparingInt(Estudiante::getNumEstudiante));
+
+        for(Estudiante est: estudiantesErasmus){
+            System.out.println(est);
+        }
+
+        System.out.println("\n ************** \n");
+        System.out.println("Ordenando por nota media:");
+
+        //Hacer ordenacion por nota media...
+        estudiantesErasmus.sort(
+                Comparator
+                        .comparingDouble(Estudiante::getNotaMedia)
+                        .thenComparing(Estudiante::getDni)
+                        .reversed());
+
+        for(Estudiante est: estudiantesErasmus){
+            System.out.println(est);
+        }
+
+        System.out.println("\n ************** \n");
+        System.out.println("Ordenando por DNI:");
+
+        estudiantesErasmus.sort(Comparator.comparing(Estudiante::getDni));
+        for(Estudiante est: estudiantesErasmus){
+            System.out.println(est);
+        }
+
     }
 }
--- src/colecciones/Estudiante.java
+++ src/colecciones/Estudiante.java
@@ -2,14 +2,29 @@ package colecciones;
 
 import java.util.Objects;
 
-public class Estudiante {
+public class Estudiante implements Comparable{
 
     private int numEstudiante;
     private String nombre;
+    private double notaMedia;
+    private String dni;
 
     public Estudiante(int numEstudiante, String nombre) {
         this.numEstudiante = numEstudiante;
         this.nombre = nombre;
+    }
+
+    public Estudiante(int numEstudiante, String nombre, double notaMedia) {
+        this.numEstudiante = numEstudiante;
+        this.nombre = nombre;
+        this.notaMedia = notaMedia;
+    }
+
+    public Estudiante(int numEstudiante, String nombre, double notaMedia, String dni) {
+        this.numEstudiante = numEstudiante;
+        this.nombre = nombre;
+        this.notaMedia = notaMedia;
+        this.dni = dni;
     }
 
     public int getNumEstudiante() {
@@ -20,11 +35,25 @@ public class Estudiante {
         return nombre;
     }
 
+    public double getNotaMedia() {
+        return notaMedia;
+    }
+
+    public void setNotaMedia(double notaMedia) {
+        this.notaMedia = notaMedia;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
     @Override
     public String toString() {
         return "Estudiante{" +
                 "numEstudiante=" + numEstudiante +
                 ", nombre='" + nombre + '\'' +
+                ", notaMedia=" + notaMedia +
+                ", dni='" + dni + '\'' +
                 '}';
     }
 
@@ -38,5 +67,13 @@ public class Estudiante {
     @Override
     public int hashCode() {
         return Objects.hashCode(numEstudiante);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Estudiante otro = (Estudiante) o;
+        //Le digo que por defecto la comparacion
+        //de estudiantes es por nombre
+        return nombre.compareTo(otro.getNombre());
     }
 }
