f85bc79016ccf121016b9a4e84021683021dab9d
diegoProFP <diego.di1@educa.madrid.org>
2026-04-24 11:04:27 +0200

    Resolucion consulta productos

--- src/accesobd/PruebaConsultas.java
+++ src/accesobd/PruebaConsultas.java
@@ -1,6 +1,8 @@
 package accesobd;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PruebaConsultas {
 
@@ -11,27 +13,59 @@ public class PruebaConsultas {
         2. Que la consulta sólo muestre los productos mayores a 100€ (es decir, pasar el parámetro, no que esté a pelo). Usar las propiedades del prepareStatement
         3. Crear clase Producto, con los mismos campos que tiene la tabla
         4. Con cada fila recuperada, crear un objeto de Producto y meter en un ArrayList
+        5. Que la consulta me devuelva tambien el nombre de la categoria del producto
+        6. Crear una clase Categoria (campos id, nombre)
+        7. Que el producto tenga un objeto de la clase Categoria
+        8. Asignar la categoria y mostrarla cuand se muestre el producto
          */
 
-        try(Connection conexion = DriverManager.getConnection(ConstantesBd.URL, ConstantesBd.USER, ConstantesBd.PASS)){
+        try(Connection conexion = DriverManager.getConnection(ConstantesBd.URL, ConstantesBd.USER, ConstantesBd.PASS);
+            PreparedStatement sentenciaConsulta = conexion.prepareStatement("SELECT * FROM productos where precio > ?");
+            ){
 
-            PreparedStatement sentenciaConsulta = conexion.prepareStatement("SELECT * FROM productos");
+            double precioFiltro = 100;
 
+            sentenciaConsulta.setDouble(1, precioFiltro);
             ResultSet conjuntoResultado = sentenciaConsulta.executeQuery();
+
+            List<Producto> productosBd = new ArrayList<>();
 
             while(conjuntoResultado.next()){
                 //Aqui estamos en la siguiente fila
+                int idProducto = conjuntoResultado.getInt("id_producto");
                 String nombreProducto = conjuntoResultado.getString("nombre");
                 double precioProducto = conjuntoResultado.getDouble("precio");
+                int stock = conjuntoResultado.getInt("stock");
+                boolean activo = conjuntoResultado.getBoolean("activo");
+                int idCategoria = conjuntoResultado.getInt("id_categoria");
 
-                System.out.println("El producto es: " + nombreProducto + " y su precio es: " + precioProducto);
+                Producto nuevo = new Producto(idProducto, nombreProducto, precioProducto,stock, activo);
+                productosBd.add(nuevo);
+//
             }
 
+            mostrarListaProductos(productosBd);
+
+
             conjuntoResultado.close();
-            sentenciaConsulta.close();
 
         }catch (SQLException e){
             System.err.println("Error general con la BD: " + e);
         }
     }
+
+    private static void mostrarListaProductos(List<Producto> productosBd) {
+
+        if(productosBd.isEmpty()){
+            System.out.println("...No hay productos...");
+        }else{
+            System.out.println("Coindencias: " + productosBd.size() + " productos");
+
+            //Linea para recorrer todos los productos e invocar a su metodo mostrarProducto
+            productosBd.forEach(Producto::mostrarProducto);
+
+        }
+    }
+
+
 }
