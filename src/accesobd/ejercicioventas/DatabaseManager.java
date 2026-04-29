908bb74c45b078d858f23c199e4b1607c01c39fe
diegoProFP <diego.di1@educa.madrid.org>
2026-04-29 12:10:11 +0200

    Empezamos con DriverManager y props

---
+++ src/accesobd/ejercicioventas/DatabaseManager.java
@@ -0,0 +1,18 @@
+package accesobd.ejercicioventas;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseManager {
+
+    private Connection conexion;
+
+    public DatabaseManager(){
+
+    }
+
+//    public Connection abrirConexion(){
+//        conexion = DriverManager.getConnection()
+//    }
+
+}
